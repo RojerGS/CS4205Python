a842f436f58af75816b421de7abeca0d46af23e4
alex <BieniekAlexander@gmail.com>
2019-05-08 15:49:35 +0200

    fixed exception thing in fitness function

--- practical1/fitness_function.py
+++ practical1/fitness_function.py
@@ -1,3 +1,9 @@
+class OptimumFoundCustomException(Exception):
+    """ The purpose of this custom exception is to perform a naughty trick: halt the GA as soon as the optimum is found
+    """
+    def __init__(self,message):
+        super().__init__(message)
+
 
 class FitnessFunction:
 
@@ -8,33 +14,26 @@ class FitnessFunction:
         self.k = k
         self.d = d
         self.evaluations = 0
-        self.optimum = m * k #TODO : this is the optimum for OneMax, not your function
+        self.optimum = m #TODO : this is the optimum for OneMax, not your function
         self.elite = None
-
-    class OptimumFoundCustomException(Exception):
-        """ The purpose of this custom exception is to perform a naughty trick: halt the GA as soon as the optimum is found
-        """
-        def __init__(self,message,errors):
-            super().__init__(message)
-            self.errors = errors
 
     def evaluate(self, individual):
         """ function to evaluate the fitness of the Individual
         """
         #TODO : You have to implement the correct evaluation function. The following is OneMax (counts 1s). Remember to modify the optimum as well
-       
+
         self.evaluations += 1
-        result = 0.0  
-        
+        result = 0.0
+
         def u(b):
-            sum(b)
-    
+            return sum(b)
+
         def fsub(b,k,d):
             if (u(b) == k):
                 return 1
             else:
                 return ((1 - d)*(k - 1 - u(b)))/(k - 1)
-      
+
         def f(x,k,m,d):
             temp = 0
             b = []
@@ -42,11 +41,11 @@ class FitnessFunction:
                 b = x[(i * k):((i+1) * k)]
                 temp += fsub(b,k,d)
             return temp
-            
+
         result = f(individual.genotype, self.k, self.m, self.d)
-   
+
         individual.fitness = result
-        
+
         if (self.elite == None or self.elite.fitness < individual.fitness):
             self.elite = individual.clone()
         if (result == self.optimum):
--- practical1/launcher.py
+++ practical1/launcher.py
@@ -1,7 +1,7 @@
 import os, logging, time
 from variation import CrossoverType, Variation
 from simple_genetic_algorithm import SimpleGeneticAlgorithm
-from fitness_function import FitnessFunction
+from fitness_function import FitnessFunction, OptimumFoundCustomException
 from itertools import product as cp
 
 def main():
@@ -13,9 +13,8 @@ def main():
         os.mkdir('experiments')
 
     population_sizes = [100]
-    ms = [8]
-    ks = [5]
-    ds = [1]
+    ms = [1, 2, 4, 8, 16]
+    ks = [2, 5, 10]
     crossoverTypes = list(CrossoverType)
 
     # set up file outputs
@@ -23,42 +22,43 @@ def main():
     f.write("crossover_type, population_size, m, k, d, generations, evaluations, time, best_fitness, optimal\n")
 
     i = -1
-    for (ct, p, m, k, d) in cp(crossoverTypes, population_sizes, ms, ks, ds):
+    for (ct, p, m, k) in cp(crossoverTypes, population_sizes, ms, ks):
+        # find the values for d to test
+        for d in [1/k, 1-1/k]:
+            # run genetic algorithm
+            i += 1
+            print("Starting run {run} with pop_size={p}, m={m}, k={k}, d={d}, crossover_type={ct}".format(
+                  run=i, p=p, m=m, k=k, d=d, ct=ct.name
+            ))
+            sga = SimpleGeneticAlgorithm(p, m, k, d, ct)
+            optimumFound = False
+            try:
+                sga.run(generations_limit, evaluations_limit, time_limit)
+            except OptimumFoundCustomException:
+                print("ewiojaoiwdj")
+                optimumFound = True
 
-        # run genetic algorithm
-        i += 1
-        print("Starting run {run} with pop_size={p}, m={m}, k={k}, d={d}, crossover_type={ct}".format(
-              run=i, p=p, m=m, k=k, d=d, ct=ct.name
-        ))
-        sga = SimpleGeneticAlgorithm(p, m, k, d, ct)
-        optimumFound = False
-        try:
-            sga.run(generations_limit, evaluations_limit, time_limit)
-        except FitnessFunction.OptimumFoundCustomException:
-            optimumFound = True
+            print("""{outcome} {bf} found at
+            generation\t{gen}
+            evaluations\t{evals}
+            time\t{time}
+            elite\t{elite}
+            """.format(outcome = "Optimum" if optimumFound == True else "Best Fitness",
+                       bf = sga.fitness_function.elite.fitness,
+                       gen = sga.generation,
+                       evals = sga.fitness_function.evaluations,
+                       time = time.time() - sga.start_time,
+                       elite = sga.fitness_function.elite
+            ))
 
-        print("""{outcome} {bf} found at
-        generation\t{gen}
-        evaluations\t{evals}
-        time\t{time}
-        elite\t{elite}
-        """.format(outcome = "Optimum" if optimumFound is False else "Best Fitness",
-                   bf = sga.fitness_function.elite.fitness,
-                   gen = sga.generation,
-                   evals = sga.fitness_function.evaluations,
-                   time = time.time() - sga.start_time,
-                   elite = sga.fitness_function.elite
-        ))
+            f.write("{ct}, {p}, {m}, {k}, {d}, {gen}, {evals}, {time}, {bf}, {found}\n".format(
+                ct = ct.name, p=p, m=m, k=k, d=d,
+                gen = sga.generation, evals = sga.fitness_function.evaluations,
+                time = time.time() - sga.start_time, bf = sga.fitness_function.elite.fitness,
+                found = optimumFound
+            ))
+            if optimumFound == True: f.write("Optimum found!\n")
 
-        f.write("{ct}, {p}, {m}, {k}, {d}, {gen}, {evals}, {time}, {bf}, {found}\n".format(
-            ct = ct.name, p=p, m=m, k=k, d=d,
-            gen = sga.generation, evals = sga.fitness_function.evaluations,
-            time = time.time() - sga.start_time, bf = sga.fitness_function.elite.fitness,
-            found = optimumFound
-        ))
-        if optimumFound == True: f.write("Optimum found!\n")
-
-    # set up logging
 
 if __name__ == "__main__":
     main()
