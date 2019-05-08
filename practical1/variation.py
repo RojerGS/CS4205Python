a2205ecdf11090e3bc89ec0069fb5de3ebd7bebf
Yakaralar <yagmurkaralar@gmail.com>
2019-05-08 15:42:48 +0200

    hey buddy

--- practical1/variation.py
+++ practical1/variation.py
@@ -1,4 +1,5 @@
 from enum import IntEnum, auto
+import random
 
 class CrossoverType(IntEnum):
     Uniform = auto()
@@ -27,7 +28,10 @@ class Variation:
         """
         child1 = parent1.clone()
         child2 = parent2.clone()
-
+        rn = randit(1,parent1.genotype_length)
+        child1.genotype[0:rn] = parent2[0:rn]
+        child2.genotype[rn:(parent1.genotype_length)] = parent1[rn:(parent1.genotype_length)]
+       
         # TODO: remember to use the rand ng to sample random numbers
 
         result = []
@@ -41,6 +45,17 @@ class Variation:
         """
         child1 = parent1.clone()
         child2 = parent2.clone()
+        
+        for i in range(parent1.genotype_length):
+            randn = random.uniform(0,1)
+            temp=0
+            if(randn >= 0.5):
+                temp=child1.genotype[i]
+                child1.genotype[i]=child2.genotype[i]
+                child2.genotype[i]= temp
+            else:
+                continue
+               
 
         # TODO: remember to use the rand ng to sample random numbers
 
