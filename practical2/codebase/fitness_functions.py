043c706b162ea193127031d806547d5450fe1b8f
alex <BieniekAlexander@gmail.com>
2019-06-07 16:44:22 +0200

    finished writing SoREB, somebody please check it for me

--- practical2/codebase/fitness_functions.py
+++ practical2/codebase/fitness_functions.py
@@ -1,58 +1,73 @@
 import numpy as np
 from math import sin, cos, pi
 
+class FunctionFactory(object):
+    """
+    Class to produce functions which will be used for the evaluation of fitness
+    in our evolutionary algorithms. The functions are outlined in
+    "Exploiting Linkage Information...".
+    """
+    def get_sphere():
+        """
+        Sphere problem.
+        """
+        return lambda x: sum(np.array(x)**2)
 
-"""
-Defining fitness functions outlined in "Exploiting Linkage Information..."
-"""
-def sphere(x):
-    """
-    Sphere problem.
-    """
-    return sum(np.array(x)**2)
+    def get_rosenbrock():
+        """
+        Rosenbrock problem.
+        """
+        return lambda x: sum([(100*(x[i+1]-x[i]**2) + (1-x[i])**2) for i in range(len(x)-1)])
 
-def rosenbrock(x):
-    """
-    Rosenbrock problem.
-    """
-    return sum([(100*(x[i+1]-x[i]**2) + (1-x[i])**2) for i in range(len(x)-1)])
+    def get_rastrigin():
+        """
+        Rastrigin problem.
+        """
+        return lambda x: (10*len(x) + sum([xi**2 - 10*cos(2*pi*xi) for xi in x]))
 
-def rastrigin(x):
-    """
-    Rastrigin problem.
-    """
-    return (10*len(x) + sum([xi**2 - 10*cos(2*pi*xi) for xi in x]))
+    def get_michalewicz():
+        """
+        Michalewicz problem.
+        """
+        return lambda x: sum([-sin(x[i]) * (sin((i+1)*x[i]**2/pi)**20) for i in range(len(x))])
 
-def michalewicz(x):
-    """
-    Michalewicz problem.
-    """
-    return sum([-sin(x[i]) * (sin((i+1)*x[i]**2/pi)**20) for i in range(len(x))])
+    def get_ellipsoid():
+        """
+        Ellipsoid problem.
+        """
+        return lambda x: sum([10**((6*i)/(len(x)-1)) * (x[i]**2) for i in range(len(x))])
 
-def ellipsoid(x):
-    """
-    Ellipsoid problem.
-    """
-    return sum([10**((6*i)/(len(x)-1)) * (x[i]**2) for i in range(len(x))])
+    def get_soreb(k=5, theta=45.0):
+        """
+        SoREB problem.
+        """
+        t = theta_rad = np.radians(theta)
+        c = np.cos; s = np.sin;
+        inner_matrix = np.array([[c(t), -s(t)], [s(t), c(t)]])
 
-def soreb(x, k=5, theta=45.0):
-    """
-    SoREB problem.
-    """
-    # TODO finish this mess of a function
-    raise Exception("This fitness function hasn't been implemented yet!")
-    if (len(x) % k != 0): raise Exception("The value of k might need to be a factor of the length of x?")
-    # check if theta should be in radians or degrees
-    # r_theta
-    # return sum([ellipsoid(r_theta(x[k*i:k*(i+1)])) for i in range(len(x)/k)])
+        final_matrix = np.identity(k)
+        for i in range(k-1):
+            mult_matrix = np.identity(k)
+            mult_matrix[i:(i+2), i:(i+2)] = inner_matrix
+            final_matrix *= mult_matrix
+
+        print(final_matrix)
+
+        ellipsoid = FunctionFactory.get_ellipsoid()
+
+        def soreb(x):
+            if (len(x) % k != 0): raise Exception("The value of k might need to be a factor of the length of x?")
+            return sum([ellipsoid(final_matrix.dot(np.array(x[k*i:k*(i+1)]))) for i in range(len(x)//k)])
+
+        return soreb
 
 
 
 if __name__ == "__main__":
     x = np.random.rand(10)
-    print(sphere(x))
-    print(rosenbrock(x))
-    print(rastrigin(x))
-    print(michalewicz(x))
-    print(ellipsoid(x))
-    # soreb(x)
+    print(FunctionFactory.get_sphere()(x))
+    print(FunctionFactory.get_rosenbrock()(x))
+    print(FunctionFactory.get_rastrigin()(x))
+    print(FunctionFactory.get_michalewicz()(x))
+    print(FunctionFactory.get_ellipsoid()(x))
+    print(FunctionFactory.get_soreb()(x))
