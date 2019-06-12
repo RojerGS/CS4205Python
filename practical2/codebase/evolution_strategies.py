c8d0c5ff8bedde9b201eb5e587cdb34f946d42ff
Tuomas Koskinen <31931159+tkoskinen1@users.noreply.github.com>
2019-06-13 01:00:30 +0200

    Removed all the nice abstractions and did it all using numpy arrays, quite a bit faster.

--- practical2/codebase/evolution_strategies.py
+++ practical2/codebase/evolution_strategies.py
@@ -9,74 +9,7 @@ import numpy as np
 
 class EvolutionStrategies(GeneticAlgorithm):
 
-    class Individual(object):
-        """
-        Abstraction of an individual of the ES population,
-        an individual is described by its position/genotype (mean of a normal distribution),
-        and the variances of said distribution which define how the individual is mutated.
-        """
-
-        def __init__(self, genome_length, fitness_function,
-                     lower_bounds, upper_bounds, mean = None, variance = None, eps_sigma = 0):
-
-            self._genome_length = genome_length
-            self._fitness_function = fitness_function
-            self._fitness = None
-            self._eps_sigma = eps_sigma
-            self._lower_bounds = lower_bounds
-            self._upper_bounds = upper_bounds
-
-            bound_sizes = [upper_bounds[i] - lower_bounds[i] for i in range(len(upper_bounds))]
-
-            if(mean is None):
-                self.mean = np.array([np.random.uniform(lower_bounds[i], upper_bounds[i]) for i in range(genome_length)])
-            else:
-                self.mean = mean
-
-            if(variance is None):
-                self.variance = np.array([np.random.uniform(0, 0.05*bound_sizes[i]) for i in range(genome_length)])
-            else:
-                self.variance = variance
-
-        def evaluate(self, genotype, index_mapping):
-
-            if genotype is not None:
-                subgenotype = extrapolate_values(subgenotype = self.mean,
-                                                 genotype = genotype, index_mapping = index_mapping)
-            else:
-                subgenotype = self.mean
-
-            self._fitness =  self._fitness_function(subgenotype)
-
-            return self._fitness
-
-        def mutate(self):
-
-                #Mutate variances
-                z = np.random.normal(0, 1/(2 * self._genome_length))
-                for i in range(self._genome_length):
-                    z_i = np.random.normal(0, 1/(2 * np.sqrt(self._genome_length)))
-                    self.variance[i] = max(self._eps_sigma, self.variance[i]*np.exp(z + z_i))
-
-                #Mutate mean
-                for i in range(self._genome_length):
-                    self.mean[i] += np.random.normal(0, self.variance[i])
-                    self.mean[i] = np.clip(self.mean[i], self._lower_bounds[i], self._upper_bounds[i])
-
-        def crossover(self, other):
-            """
-            Crossover this individual with another one to produce single offspring
-            """
-            tmp = np.random.randint(0,1,self._genome_length)
-            offspring_mean = np.multiply(self.mean, tmp) + np.multiply(other.mean, np.abs(tmp-1))
-            offspring_variance = 0.5*(self.variance + other.variance)
-
-            offspring = EvolutionStrategies.Individual(self._genome_length, self._fitness_function,
-                                                       self._lower_bounds, self._upper_bounds,
-                                                       offspring_mean, offspring_variance)
-            return offspring
-
-    def __init__(self, fitness_function, genome_length, population_size=25,
+    def __init__(self, fitness_function, genome_length, population_size=20,
                  lower_bounds=-3.0, upper_bounds=3.0, initial_genotype = None, index_mapping = None,
                  max_generations = float('inf'),  max_evaluations = float('inf'), goal_fitness = float('-inf'),*,
                  offspring_size = None):
@@ -94,8 +27,27 @@ class EvolutionStrategies(GeneticAlgorithm):
               upper_bounds = upper_bounds,
               lower_bounds = lower_bounds)
 
-        self._offspring_size = 5*population_size if offspring_size is None else offspring_size
+        self._max_generations = max_generations
+        self._max_evaluations = max_evaluations
+        self._goal_fitness = goal_fitness
+        self._best_finess = float('inf')
+        self._best_genotype = None
+        self._offspring_size = 7*population_size if offspring_size is None else offspring_size
         self.init_population(initial_genotype, index_mapping)
+
+    def evaluate(self, mean, genotype=None, index_mapping=None):
+        """
+        reevaluate the fitness of the particle, accounting for additional values
+        that need to be used in evaluation as necessary.
+        """
+        if genotype is not None:
+            subgenotype = extrapolate_values(subgenotype = self._curr_position,
+                                             genotype = genotype, index_mapping = index_mapping)
+        else:
+            subgenotype = mean
+
+        self._evaluations += 1
+        return self._fitness_function(subgenotype)
 
 
     def init_population(self, initial_genotype = None, index_mapping = None):
@@ -108,12 +60,13 @@ class EvolutionStrategies(GeneticAlgorithm):
             index_mapping (IndexMapping): object storing the indexing from the space of the
             genotypes in a Cooperative Coevolution to the train space.
         """
-        self._population = [EvolutionStrategies.Individual(self._genome_length, self._fitness_function, self._lower_bounds, self._upper_bounds)
-                            for _ in range(self._population_size)]
+        self._population_means = np.random.uniform(self._lower_bounds, self._upper_bounds, size=(self._population_size, self._genome_length))
+        self._population_fitnesses = np.array([self.evaluate(x, initial_genotype, index_mapping) for x in self._population_means])
+        #self._population_variances = np.random.uniform(0, 0.1*(self._upper_bounds - self._lower_bounds), size=(self._population_size, self._genome_length))
+        self._population_variances = np.random.uniform(0.01*(self._upper_bounds - self._lower_bounds), 0.05*(self._upper_bounds - self._lower_bounds), size=(self._population_size, self._genome_length))
 
-        for i in self._population:
-            i.evaluate(initial_genotype, index_mapping)
-
+        self._best_fitness = np.min(self._population_fitnesses)
+        self._best_genotype = self._population_means[np.argmin(self._population_fitnesses)]
 
     def evolve(self, genotype=None, index_mapping=None):
         """
@@ -126,59 +79,103 @@ class EvolutionStrategies(GeneticAlgorithm):
             genotypes in a Cooperative Coevolution to the train space.
         """
         self._generations += 1
-        self._evaluations += self._offspring_size
 
-        offspring = []
+        #Randomly select parent from current population, parent_set_1[i] and parent_set_2[i] are
+        #recombined to get offspring number i
+        parent_set_1 = np.random.randint(0, self._population_size, size = self._offspring_size)
+        parent_set_2 = np.random.randint(0, self._population_size, size = self._offspring_size)
 
-        for _ in range(self._offspring_size):
-            parent1, parent2 = np.random.choice(self._population, 2)
-            child = parent1.crossover(parent2)
-            child.mutate()
-            child.evaluate(genotype, index_mapping)
-            offspring.append(child)
+        #Get means of parents
+        parent_means_1 = self._population_means[parent_set_1]
+        parent_means_2 = self._population_means[parent_set_2]
 
-        self._population = sorted(self._population + offspring, key = lambda x: x._fitness)[:self._population_size]
+        #Get variances of parents
+        parent_variances_1 = self._population_variances[parent_set_1]
+        parent_variances_2 = self._population_variances[parent_set_2]
+
+        #Recombine means of parents
+        recombination_matrix = np.random.randint(0, 1, size = (self._offspring_size, self._genome_length))
+        offspring_means = np.multiply(parent_means_1, recombination_matrix) - np.multiply(parent_means_2, recombination_matrix - 1)
+
+        #Recombine variances of parents
+        offspring_variances = 0.5*(parent_variances_1 + parent_variances_2)
+
+        #Mutate offspring variances
+        z_vector = np.random.normal(0, np.sqrt(1/(2 * self._genome_length)), size = (self._offspring_size, 1))
+        zi_matrix = np.random.normal(0, np.sqrt(1/(2 * np.sqrt(self._genome_length))), size = (self._offspring_size, self._genome_length))
+        offspring_variances = np.multiply(offspring_variances, np.exp(zi_matrix + z_vector))
+
+        #Mutate offspring means
+        offspring_means += np.array([np.random.multivariate_normal(mean = np.zeros(self._genome_length), cov = np.diag(var)) for var in offspring_variances])
+        offspring_fitnesses = np.array([self.evaluate(x, genotype, index_mapping) for x in offspring_means])
+
+        """
+        all_fitnesses = np.concatenate((self._population_fitnesses, offspring_fitnesses))
+        all_means = np.concatenate((self._population_means, offspring_means))
+        all_variances = np.concatenate((self._population_variances, offspring_variances))
+        most_fit = np.argsort(all_fitnesses)[:self._population_size]
+        self._population_means = all_means[most_fit]
+        self._population_variances = all_variances[most_fit]
+        self._population_fitnesses = all_fitnesses[most_fit]
+        """
+
+        #Perform selection among offspring
+        most_fit = np.argsort(offspring_fitnesses)[:self._population_size]
+        self._population_means = offspring_means[most_fit]
+        self._population_variances = offspring_variances[most_fit]
+        self._population_fitnesses = offspring_fitnesses[most_fit]
+
+        if(self._population_fitnesses[0] < self._best_fitness):
+            self._best_fitness = self._population_fitnesses[0]
+            self._best_genotype = self._population_means[0,:]
 
     def has_converged(self):
         """
         check if our population has met some convergence condition.
         """
-        return False
+        return (self._generations >= self._max_generations
+                or self._evaluations >= self._max_evaluations
+                or self.get_best_fitness() <= self._goal_fitness)
 
     def get_best_genotypes(self, n=1):
         """
         Return the genotype of the best n individuals in the population.
         """
-        return [ind.mean for ind in sorted(self._population, key = lambda x: x._fitness)[:n]]
+        tmp = np.argsort(self._population_fitnesses)[:n]
+
+        return self._population_means[tmp]
 
     def get_best_fitness(self, n=1):
         """
         Return the best n values of the fitness.
         """
-        return [ind._fitness for ind in sorted(self._population, key = lambda x: x._fitness)[:n]]
+        if(n == 1):
+            return [self._best_fitness]
+
+        return sorted(self._population_fitnesses)[:n]
 
 if __name__ == "__main__":
     from fitness_functions import FunctionFactory as FF
     from matplotlib import pyplot as plt
 
-    f = FF.get_sphere()
-    #f = FF.get_rosenbrock()
+    #f = FF.get_sphere()
+    f = FF.get_rosenbrock()
     #f = FF.get_soreb()
 
     pop = EvolutionStrategies(fitness_function = f,
-                                genome_length = 10,
-                                population_size = 100,
-                                offspring_size = 100,
-                                lower_bounds=-10, upper_bounds=10)
+                                genome_length = 3,
+                                population_size=10,
+                                lower_bounds=-100, upper_bounds=100)
+
 
     fit = []
-    for i in range(100):
-        fit.append(pop.get_best_fitness(1)[0])
+
+    for _ in range(1000):
         pop.evolve()
+        fit.append(pop.get_best_fitness(1)[0])
 
-    fit.append(pop.get_best_fitness(1)[0])
-
-    print(pop.get_best_genotypes(1)[0])
-    print(pop.get_best_fitness(1)[0])
+    print(pop.get_best_genotypes(10))
+    print(pop.get_best_fitness(10))
+    print(pop._best_fitness)
     plt.plot(fit)
     plt.show()
